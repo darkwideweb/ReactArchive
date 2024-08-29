a28f8fffd2549aa9ed263d1c22301292bee772f6
nikita shtukaturov <150450540+darkwideweb@users.noreply.github.com>
2024-08-29 17:13:23 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,41 +9,16 @@ const Home = () => {
   const fullTitle = '   Добро пожаловать в интерактивный учебник';
   const theme = useTheme();
   const isSmDown = useMediaQuery(theme.breakpoints.down('sm'));
-  
   const currentIndexRef = useRef(0);
-  
+
   const sections = [
-    {
-      title: 'Галерея компонентов',
-      description: 'Откройте для себя примеры использования различных React-компонентов с Material UI — от Google.',
-      link: '/components',
-    },
-    {
-      title: 'Интерактивные викторины',
-      description: 'Проверьте свои знания, проходя увлекательные викторины по React.',
-      link: '/quizzes',
-    },
-    {
-      title: 'Полезные ресурсы',
-      description: 'Доступ к актуальным и полезным ресурсам по React и веб-разработке.',
-      link: '/resources',
-    },
-    {
-      title: 'Проектные работы',
-      description: 'Идея и реализация для первого проекта на React.',
-      link: '/project-labs',
-    },
-    {
-      title: 'Интерактивный IDE',
-      description: 'Попробуйте написание и тестирование кода прямо в браузере.',
-      link: '/code-editor',
-    },
-    {
-      title: 'Креативные челленджи',
-      description: 'Примите участие в креативных челленджах.',
-      link: '/creative-challenges',
-    },
-  ];  
+    { title: 'Галерея компонентов', description: 'Откройте для себя примеры использования различных React-компонентов с Material UI.', link: '/components' },
+    { title: 'Интерактивные викторины', description: 'Проверьте свои знания, проходя увлекательные викторины по React.', link: '/quizzes' },
+    { title: 'Полезные ресурсы', description: 'Доступ к актуальным и полезным ресурсам по React и веб-разработке.', link: '/resources' },
+    { title: 'Проектные работы', description: 'Идея и реализация для первого проекта на React.', link: '/project-labs' },
+    { title: 'Интерактивный IDE', description: 'Попробуйте написание и тестирование кода прямо в браузере.', link: '/code-editor' },
+    { title: 'Креативные челленджи', description: 'Примите участие в креативных челленджах.', link: '/creative-challenges' },
+  ];
 
   useEffect(() => {
     const savedProgress = localStorage.getItem('progress');
@@ -59,13 +34,12 @@ const Home = () => {
     };
 
     const typingInterval = setInterval(typeTitle, 50);
-
     return () => clearInterval(typingInterval);
   }, [fullTitle]);
 
   const handleSectionVisit = useCallback(() => {
     setProgress((prevProgress) => {
-      const newProgress = Math.min(prevProgress + 5, 100); 
+      const newProgress = Math.min(prevProgress + 10, 100);
       localStorage.setItem('progress', newProgress);
       return newProgress;
     });
@@ -82,127 +56,125 @@ const Home = () => {
         boxShadow: '0 20px 40px rgba(0,0,0,0.1)',
       }}
     >
-    
-<motion.div
-  initial={{ opacity: 0, y: -50 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 1.2, type: 'spring', stiffness: 70 }}
-  whileHover={{ scale: 1.05 }}
-  style={{ textAlign: 'center', marginBottom: '40px', padding: '0 20px' }}
->
-  <Typography
-    variant={isSmDown ? 'h4' : 'h2'}
-    gutterBottom
-    sx={{
-      fontWeight: 'bold',
-      color: 'text.primary',
-      textShadow: '4px 4px 10px rgba(0,0,0,0.3)',
-      mb: 3,
-      fontFamily: "'Roboto Slab', serif",
-      whiteSpace: 'pre-wrap',
-      transition: 'color 0.5s, text-shadow 0.5s',
-      background: 'linear-gradient(90deg, #42a5f5, #1976d2)',
-      WebkitBackgroundClip: 'text',
-      WebkitTextFillColor: 'transparent',
-      animation: 'gradientShift 4s ease infinite',
-      '@keyframes gradientShift': {
-        '0%': { backgroundPosition: '0% 50%' },
-        '50%': { backgroundPosition: '100% 50%' },
-        '100%': { backgroundPosition: '0% 50%' },
-      },
-      '&:hover': {
-        color: 'primary.main',
-        textShadow: '5px 5px 15px rgba(0,0,0,0.4)',
-      },
-    }}
-  >
-    {typedTitle}
-  </Typography>
-  <Typography
-    variant="h5"
-    sx={{
-      color: 'text.secondary',
-      fontFamily: "'Open Sans', sans-serif",
-      transition: 'color 0.5s, transform 0.3s',
-      '&:hover': {
-        color: 'text.primary',
-        transform: 'translateY(-5px)',
-      },
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-  >
-    <EmojiEventsIcon 
-      sx={{ 
-        verticalAlign: 'middle', 
-        mr: 1, 
-        fontSize: 'inherit', 
-        color: 'primary.main', 
-        transition: 'transform 0.3s',
-        '&:hover': { 
-          transform: 'rotate(20deg)', 
-          color: 'secondary.main' 
-        } 
-      }} 
-    />
-    Исследуйте и учитесь в собственном темпе
-  </Typography>
-</motion.div>
-
-<Grid container spacing={4} justifyContent="center" sx={{ mt: 4 }}>
-  <Grid item xs={12} sm={10} md={8}>
-    <Box>
       <motion.div
-        initial={{ opacity: 0, width: 0 }}
-        animate={{ opacity: 1, width: `${progress}%` }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, type: 'spring', stiffness: 70 }}
+        whileHover={{ scale: 1.05 }}
+        style={{ textAlign: 'center', marginBottom: '40px', padding: '0 20px' }}
       >
-        <LinearProgress
-          variant="determinate"
-          value={progress}
-          sx={{
-            height: 14,
-            borderRadius: 8,
-            backgroundColor: '#f5f5f5',
-            '& .MuiLinearProgress-bar': {
-              borderRadius: 8,
-              backgroundColor: 'primary.main',
-              backgroundImage: 'linear-gradient(90deg, #42a5f5, #64b5f6)',
-              boxShadow: `0 0 8px 0 rgba(66, 165, 245, 0.5)`,
-              transition: 'width 0.8s ease-in-out, background-color 0.5s',
-              '&:hover': {
-                backgroundColor: 'secondary.main',
-                backgroundImage: 'linear-gradient(90deg, #ef5350, #e57373)',
-              },
-            },
-          }}
-        />
-      </motion.div>
-      <Box sx={{ mt: 2, textAlign: 'center' }}>
         <Typography
-          variant="h6"
+          variant={isSmDown ? 'h4' : 'h2'}
+          gutterBottom
           sx={{
             fontWeight: 'bold',
             color: 'text.primary',
-            textShadow: '2px 2px 4px rgba(0,0,0,0.2)',
-            letterSpacing: '0.5px',
-            transition: 'color 0.3s, text-shadow 0.3s',
+            textShadow: '4px 4px 10px rgba(0,0,0,0.3)',
+            mb: 3,
+            fontFamily: "'Roboto Slab', serif",
+            whiteSpace: 'pre-wrap',
+            transition: 'color 0.5s, text-shadow 0.5s',
+            background: 'linear-gradient(90deg, #42a5f5, #1976d2)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            animation: 'gradientShift 4s ease infinite',
+            '@keyframes gradientShift': {
+              '0%': { backgroundPosition: '0% 50%' },
+              '50%': { backgroundPosition: '100% 50%' },
+              '100%': { backgroundPosition: '0% 50%' },
+            },
             '&:hover': {
               color: 'primary.main',
-              textShadow: '2px 2px 6px rgba(0,0,0,0.3)',
+              textShadow: '5px 5px 15px rgba(0,0,0,0.4)',
             },
           }}
         >
-          Прогресс: {progress}%
+          {typedTitle}
         </Typography>
-      </Box>
-    </Box>
-  </Grid>
-</Grid>
+        <Typography
+          variant="h5"
+          sx={{
+            color: 'text.secondary',
+            fontFamily: "'Open Sans', sans-serif",
+            transition: 'color 0.5s, transform 0.3s',
+            '&:hover': {
+              color: 'text.primary',
+              transform: 'translateY(-5px)',
+            },
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <EmojiEventsIcon
+            sx={{
+              verticalAlign: 'middle',
+              mr: 1,
+              fontSize: 'inherit',
+              color: 'primary.main',
+              transition: 'transform 0.3s',
+              '&:hover': {
+                transform: 'rotate(20deg)',
+                color: 'secondary.main',
+              },
+            }}
+          />
+          Исследуйте и учитесь в собственном темпе
+        </Typography>
+      </motion.div>
 
-      <Grid container spacing={4} 
-Content="center" sx={{ mt: 4 }}>
+      <Grid container spacing={4} justifyContent="center" sx={{ mt: 4 }}>
+        <Grid item xs={12} sm={10} md={8}>
+          <Box>
+            <motion.div
+              initial={{ opacity: 0, width: 0 }}
+              animate={{ opacity: 1, width: `${progress}%` }}
+              transition={{ duration: 1, ease: 'easeInOut' }}
+            >
+              <LinearProgress
+                variant="determinate"
+                value={progress}
+                sx={{
+                  height: 14,
+                  borderRadius: 8,
+                  backgroundColor: '#f5f5f5',
+                  '& .MuiLinearProgress-bar': {
+                    borderRadius: 8,
+                    backgroundColor: 'primary.main',
+                    backgroundImage: 'linear-gradient(90deg, #42a5f5, #64b5f6)',
+                    boxShadow: `0 0 8px 0 rgba(66, 165, 245, 0.5)`,
+                    transition: 'width 0.8s ease-in-out, background-color 0.5s',
+                    '&:hover': {
+                      backgroundColor: 'secondary.main',
+                      backgroundImage: 'linear-gradient(90deg, #ef5350, #e57373)',
+                    },
+                  },
+                }}
+              />
+            </motion.div>
+            <Box sx={{ mt: 2, textAlign: 'center' }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  fontWeight: 'bold',
+                  color: 'text.primary',
+                  textShadow: '2px 2px 4px rgba(0,0,0,0.2)',
+                  letterSpacing: '0.5px',
+                  transition: 'color 0.3s, text-shadow 0.3s',
+                  '&:hover': {
+                    color: 'primary.main',
+                    textShadow: '2px 2px 6px rgba(0,0,0,0.3)',
+                  },
+                }}
+              >
+                Прогресс: {progress}%
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={4} justifyContent="center" sx={{ mt: 4 }}>
         <Grid item xs={12} md={6}>
           <Paper
             component={motion.div}
@@ -230,7 +202,7 @@ Content="center" sx={{ mt: 4 }}>
               О проекте
             </Typography>
             <Typography variant="body1" sx={{ mb: 2, color: 'text.secondary' }}>
-              Этот проект представляет собой интерактивный учебник для изучения React. Здесь вы найдете уроки, викторины, задачи. челленджи и др. которые помогут вам укрепить знания в React и веб-разработке.
+              Этот проект представляет собой интерактивный учебник для изучения React. Здесь вы найдете уроки, викторины, задачи, челленджи и др., которые помогут вам укрепить знания в React и веб-разработке.
             </Typography>
             <Button
               variant="contained"
@@ -282,7 +254,7 @@ Content="center" sx={{ mt: 4 }}>
               Уроки
             </Typography>
             <Typography variant="body1" sx={{ mb: 2, color: 'text.secondary' }}>
-              Я старался продумать каждый урок добавляя к ним различные примеры и вопросы для самоконтроля. В первую очередь этот учебник я выстраивал под себя, но очень надеюсь, что он поможет и другим.
+              Я старался продумать каждый урок, добавляя к ним различные примеры и вопросы для самоконтроля. В первую очередь этот учебник я выстраивал под себя, но очень надеюсь, что он поможет и другим.
             </Typography>
             <Button
               variant="contained"
@@ -311,66 +283,67 @@ Content="center" sx={{ mt: 4 }}>
 
       <Grid container spacing={4} justifyContent="center" sx={{ mt: 4 }}>
         {sections.map((section) => (
-  <Grid item xs={12} md={6} lg={4} key={section.title}>
-    <Card
-      component={motion.div}
-      whileHover={{ scale: 1.05, rotate: 2 }}
-      transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}
-      sx={{
-        mb: 4,
-        padding: 0,
-        overflow: 'hidden',
-        borderRadius: '20px',
-        boxShadow: '0 20px 40px rgba(0,0,0,0.15)',
-        transition: 'transform 0.3s, box-shadow 0.3s',
-        '&:hover': {
-          transform: 'translateY(-5px)',
-          boxShadow: '0 30px 50px rgba(0,0,0,0.2)',
-        },
-      }}
-    >
-      <CardContent sx={{ padding: 3 }}>
-        <Typography
-          variant="h6"
-          sx={{
-            fontWeight: 'bold',
-            mb: 1,
-            color: 'text.primary',
-            fontFamily: "'Roboto Slab', serif",
-          }}
-        >
-          {section.title}
-        </Typography>
-        <Typography variant="body1" sx={{ color: 'text.secondary' }}>
-          {section.description}
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          href={section.link}
-          onClick={handleSectionVisit}
-          sx={{
-            mt: 2,
-            padding: '10px 20px',
-            borderRadius: '50px',
-            backgroundImage: 'linear-gradient(45deg, #42a5f5 30%, #64b5f6 90%)',
-            boxShadow: '0 4px 10px rgba(66, 165, 245, 0.3)',
-            transition: 'background-image 0.3s, box-shadow 0.3s',
-            '&:hover': {
-              backgroundImage: 'linear-gradient(45deg, #64b5f6 30%, #42a5f5 90%)',
-              boxShadow: '0 6px 15px rgba(66, 165, 245, 0.5)',
-            },
-          }}
-        >
-          Узнать больше
-        </Button>
-      </CardContent>
-      </Card>
-    </Grid>
-    ))}
+          <Grid item xs={12} md={6} lg={4} key={section.title}>
+            <Card
+              component={motion.div}
+              whileHover={{ scale: 1.05, rotate: 2 }}
+              transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}
+              sx={{
+                mb: 4,
+                padding: 0,
+                overflow: 'hidden',
+                borderRadius: '20px',
+                boxShadow: '0 20px 40px rgba(0,0,0,0.15)',
+                transition: 'transform 0.3s, box-shadow 0.3s',
+                '&:hover': {
+                  transform: 'translateY(-5px)',
+                  boxShadow: '0 30px 50px rgba(0,0,0,0.2)',
+                },
+              }}
+            >
+              <CardContent sx={{ padding: 3 }}>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    fontWeight: 'bold',
+                    mb: 1,
+                    color: 'text.primary',
+                    fontFamily: "'Roboto Slab', serif",
+                  }}
+                >
+                  {section.title}
+                </Typography>
+                <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+                  {section.description}
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  href={section.link}
+                  onClick={handleSectionVisit}
+                  sx={{
+                    mt: 2,
+                    padding: '10px 20px',
+                    borderRadius: '50px',
+                    backgroundImage: 'linear-gradient(45deg, #42a5f5 30%, #64b5f6 90%)',
+                    boxShadow: '0 4px 10px rgba(66, 165, 245, 0.3)',
+                    transition: 'background-image 0.3s, box-shadow 0.3s',
+                    '&:hover': {
+                      backgroundImage: 'linear-gradient(45deg, #64b5f6 30%, #42a5f5 90%)',
+                      boxShadow: '0 6px 15px rgba(66, 165, 245, 0.5)',
+                    },
+                  }}
+                >
+                  Узнать больше
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
 };
 
 export default Home;
+
